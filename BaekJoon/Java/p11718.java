2a987b65a39b271bfc97e5da9d9efa1f66ee8953
Suyeon <tndus970120@gmail.com>
2020-01-25 23:41:43 -0500

    print p11718

---
+++ BaekJoon/Java/p11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class p11718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNextLine()) {
+			System.out.println(scan.nextLine());
+		}
+
+	}
+
+}
