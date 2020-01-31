9df5362194cbebac89b172432e878368a35366a7
Suyeon <tndus970120@gmail.com>
2020-01-31 00:14:21 -0500

    p10869

---
+++ BaekJoon/Java/p10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class p10869 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+
+}
