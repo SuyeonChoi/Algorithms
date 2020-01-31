d56b62130227b898122c5ab6eef579a056029dd4
Suyeon <tndus970120@gmail.com>
2020-01-31 00:19:56 -0500

    p10430

---
+++ BaekJoon/Java/p10430.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class p10430 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		int C = scan.nextInt();
+		System.out.println((A+B)%C);
+		System.out.println((A%C + B%C)%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+	}
+
+}
