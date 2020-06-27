174ee4c382faf3090877e516c57b36059204865f
SuyeonChoi <tndus970120@gmail.com>
2020-06-27 23:11:47 +0900

    p10845

---
+++ BaekJoon/Python/p10845.py
@@ -0,0 +1,35 @@
+from sys import stdin
+num = int(input())
+queue = list()
+
+
+for i in range(num):
+    command = stdin.readline().split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            element = queue[0]
+            del queue[0]
+            print(element)
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
+    else:
+        pass
