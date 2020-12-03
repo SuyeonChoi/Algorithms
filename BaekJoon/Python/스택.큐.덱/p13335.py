013cd9347b61c19ceb2f6e9836059529cb4c6580
SuyeonChoi <tndus970120@gmail.com>
2020-12-03 22:09:39 +0900

    트럭 using Stack

---
+++ BaekJoon/Python/스택.큐.덱/p13335.py
@@ -0,0 +1,31 @@
+def solution(bridge_length, weight, truck_weights):
+    stack = []
+    tmp = 0
+    time = 0
+    while truck_weights:
+        if stack and stack[0][1] == bridge_length:
+            truck = stack.pop(0)
+            tmp -= truck[0]
+        if (tmp + truck_weights[0]) <= weight:
+            truck = truck_weights.pop(0)
+            tmp += truck
+            stack.append([truck, 0])
+        for j in range(len(stack)):
+            stack[j][1] += 1
+        # print(stack)
+        time += 1
+
+    while stack:
+        if stack[0][1] == bridge_length:
+            stack.pop(0)
+        for j in range(len(stack)):
+            stack[j][1] += 1
+        time += 1
+        # print(stack)
+    return time
+
+import sys
+input = sys.stdin.readline
+n, w, L = map(int, input().split())
+trucks = list(map(int, input().split()))
+print(solution(w, L, trucks))
