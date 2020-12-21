c812409b1a611a5c895c7b9d914356af6bd3d49c
SuyeonChoi <tndus970120@gmail.com>
2020-12-21 15:43:10 +0900

    2차원 배열의합(2) using DP

--- BaekJoon/Python/Dynamic Programming/p2167.py
+++ BaekJoon/Python/Dynamic Programming/p2167.py
@@ -1,20 +1,16 @@
-
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 arr = []
 for i in range(N):
     arr.append(list(map(int, input().split())))
 
 K = int(input())
-arrSum = []
+dp = [[0]*(M+1) for i in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+# print(dp)
 for k in range(K):
     i, j, x, y = map(int, input().split())
-    _sum = 0
-    curR, curC = i-1, j-1
-    while curR <= x and curC <= y:
-        if curC == (M-1):
-            curC = 0
-        _sum += arr[curR][curC]
-        curR += 1
-        curC += 1
-    arrSum.append(_sum)
-print(arrSum)
+    print(dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1])
