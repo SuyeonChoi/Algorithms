8d9a898b895b31d15f34f040893fe4c0774897bb
SuyeonChoi <tndus970120@gmail.com>
2020-12-23 22:11:52 +0900

    덩치(2) using BruteForce

--- BaekJoon/Python/p7568.py
+++ BaekJoon/Python/p7568.py
@@ -1,10 +1,17 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 arr = []
 ans = []
 for i in range(N):
     x, y = map(int, input().split())
     arr.append([x, y])
-arr = sorted(arr, key=lambda x: -x[0])
-
-
-print(arr)
+for i in range(N):
+    rank = 0
+    for j in range(N):
+        if i == j:
+            continue
+        if arr[i][1] < arr[j][1] and arr[i][0] < arr[j][0]:
+            rank += 1
+    ans.append(rank+1)
+print(' '.join(map(str, ans)))
