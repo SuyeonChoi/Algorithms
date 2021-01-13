dfdd9f5e51df49cb56f3aeb167422df5040297d3
SuyeonChoi <tndus970120@gmail.com>
2021-01-13 23:56:48 +0900

    [S/W 문제해결 기본]9일차-사칙연산 유효성 검사

---
+++ SW Expert Academy/Java/p1233.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class p1233 {
+    static int[] lc;
+    static int[] rc;
+    static String[] alp;
+    static LinkedList<String> exp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int t = 1; t < 11; t++) {
+            int N = Integer.parseInt(br.readLine());
+            lc = new int[N + 1];
+            rc = new int[N + 1];
+            alp = new String[N + 1];
+            exp = new LinkedList<>();
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                alp[u] = st.nextToken();
+                if (st.hasMoreTokens()) {
+                    lc[u] = Integer.parseInt(st.nextToken());
+                }
+                if (st.hasMoreTokens()) {
+                    rc[u] = Integer.parseInt(st.nextToken());
+                }
+            }
+            inorder(1);
+            Stack<String> stack = new Stack<>();
+            int flag = 1;
+            for (int i = 0; i < exp.size(); i++) {
+                if (exp.get(i).equals(")")) {
+                    int num1, num2;
+                    String op;
+                    //num2확인
+                    if (stack.peek().equals("+") || stack.peek().equals("-") || stack.peek().equals("*") || stack.peek().equals("/")) {
+                        flag = 0;
+                        break;
+                    } else {
+                        num2 = Integer.parseInt(stack.pop());
+                    }
+
+                    //op 확인
+                    if (stack.peek().equals("+") || stack.peek().equals("-") || stack.peek().equals("*") || stack.peek().equals("/")) {
+                        op = stack.pop();
+                    } else {
+                        flag = 0;
+                        break;
+                    }
+
+                    //num1 확인
+                    if (stack.peek().equals("+") || stack.peek().equals("-") || stack.peek().equals("*") || stack.peek().equals("/")) {
+                        flag = 0;
+                        break;
+                    } else {
+                        num1 = Integer.parseInt(stack.pop());
+                    }
+                    stack.pop();
+                    if (op.equals("+")) stack.add(String.valueOf(num1 + num2));
+                    else if (op.equals("-")) stack.add(String.valueOf(num1 - num2));
+                    else if (op.equals("*")) stack.add(String.valueOf(num1 * num2));
+                    else if (op.equals("/")) {
+                        if(num2 == 0){
+                            stack.add("1");
+                        }else{
+                            stack.add(String.valueOf(num1 / num2));
+                        }
+                    }
+
+                } else {
+                    stack.add(exp.get(i));
+                }
+            }
+            System.out.println("#" + t + " " + flag);
+        }
+    }
+
+    private static void inorder(int cur) {
+        if (lc[cur] != 0) {
+            exp.add("(");
+            inorder(lc[cur]);
+        }
+        exp.add(alp[cur]);
+        if (rc[cur] != 0) {
+            inorder(rc[cur]);
+            exp.add(")");
+        }
+    }
+}
