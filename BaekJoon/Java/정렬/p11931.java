423d4fa6724fad3a9daa2e448ef27b24a924556f
SuyeonChoi <tndus970120@gmail.com>
2021-01-21 23:54:24 +0900

    수 정렬하기4 using Java Arrays.sort method

---
+++ BaekJoon/Java/정렬/p11931.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class p11931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        Collections.reverse(list);
+        for (int i : list) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
