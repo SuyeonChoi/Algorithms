f893708b6a9ed0c070fc729d6a99459caa851d55
SuyeonChoi <tndus970120@gmail.com>
2021-03-15 00:35:52 +0900

    탈주범 검거 using BFS

--- SW Expert Academy/Java/BFS.DFS/p1953.java
+++ SW Expert Academy/Java/BFS.DFS/p1953.java
@@ -56,21 +56,21 @@ public class p1953 {
                 int x = cur.x;
                 int y = cur.y;
                 sum++;
-                if(vis[x][y] == L) continue;
+                if (vis[x][y] == L) continue;
                 int nx, ny;
                 switch (map[cur.x][cur.y]) {
                     case 1:
                         for (int dir = 0; dir < 4; dir++) {
                             nx = x + dx[dir];
                             ny = y + dy[dir];
-                            if(checkTunnel(nx, ny, x, y)){
-                                if(dir == 0){
+                            if (checkTunnel(nx, ny, x, y)) {
+                                if (dir == 0) {
                                     checkUp(nx, ny, x, y);
-                                }else if(dir == 1){
+                                } else if (dir == 1) {
                                     checkDown(nx, ny, x, y);
-                                }else if(dir == 2){
+                                } else if (dir == 2) {
                                     checkLeft(nx, ny, x, y);
-                                }else{
+                                } else {
                                     checkRight(nx, ny, x, y);
                                 }
                             }
@@ -79,99 +79,106 @@ public class p1953 {
                     case 2:
                         for (int dir = 0; dir < 2; dir++) {
                             nx = x + dx[dir];
-                            if(checkTunnel(nx, y, x, y)){
-                            if(dir == 0){
-                                checkUp(nx, y, x, y);
-                            }else{
-                                checkDown(nx, y, x, y);
-                            }}
+                            if (checkTunnel(nx, y, x, y)) {
+                                if (dir == 0) {
+                                    checkUp(nx, y, x, y);
+                                } else {
+                                    checkDown(nx, y, x, y);
+                                }
+                            }
                         }
                         break;
                     case 3:
                         for (int dir = 2; dir < 4; dir++) {
                             ny = y + dy[dir];
-                            if(checkTunnel(nx, ny, x, y)){
-                            if(dir == 0){
-                                checkLeft(x, ny, x, y);
-                            }else{
-                                checkRight(x, ny, x, y);
+                            if (checkTunnel(x, ny, x, y)) {
+                                if (dir == 2) {
+                                    checkLeft(x, ny, x, y);
+                                } else {
+                                    checkRight(x, ny, x, y);
+                                }
                             }
                         }
                         break;
                     case 4:
                         //상
-                        nx = x + 1;
-                        checkTunnel(nx, y, x, y);
-                        checkUp(nx, y, x, y);
+                        nx = x - 1;
+                        if (checkTunnel(nx, y, x, y)) {
+                            checkUp(nx, y, x, y);
+                        }
                         //우
                         ny = y + 1;
-                        checkTunnel(x, ny, x, y);
-                        checkRight(x, ny, x, y);
+                        if (checkTunnel(x, ny, x, y)) {
+                            checkRight(x, ny, x, y);
+                        }
                         break;
                     case 5:
                         //하
-                        nx = x - 1;
-                        checkTunnel(nx, y, x, y);
-                        checkDown(nx, y, x, y);
+                        nx = x + 1;
+                        if (checkTunnel(nx, y, x, y)) {
+                            checkDown(nx, y, x, y);
+                        }
                         //우
                         ny = y + 1;
-                        checkTunnel(x, ny, x, y);
-                        checkRight(x, ny, x, y);
+                        if (checkTunnel(x, ny, x, y)) {
+                            checkRight(x, ny, x, y);
+                        }
                         break;
                     case 6:
                         //하
-                        nx = x - 1;
-                        checkTunnel(nx, y, x, y);
-                        checkDown(nx, y, x, y);
+                        nx = x + 1;
+                        if (checkTunnel(nx, y, x, y)) {
+                            checkDown(nx, y, x, y);
+                        }
                         //좌
                         ny = y - 1;
-                        checkTunnel(x, ny, x, y);
-                        checkLeft(x, ny, x, y);
+                        if (checkTunnel(x, ny, x, y)) {
+                            checkLeft(x, ny, x, y);
+                        }
                         break;
                     case 7:
                         //상
-                        nx = x + 1;
-                        checkUp(nx, y, x, y);
-                        checkTunnel(nx, y, x, y);
+                        nx = x - 1;
+                        if (checkTunnel(nx, y, x, y)) {
+                            checkUp(nx, y, x, y);
+                        }
                         //좌
                         ny = y - 1;
-                        checkTunnel(x, ny, x, y);
-                        checkRight(x, ny, x, y);
+                        if (checkTunnel(x, ny, x, y)) {
+                            checkLeft(x, ny, x, y);
+                        }
                         break;
                 }
-            }
-            for(int i = 0; i < N; i++){
-                System.out.println(Arrays.toString(vis[i]));
             }
             System.out.println("#" + t + " " + sum);
         }
     }
 
     private static void checkRight(int nx, int ny, int x, int y) {
-        if(map[nx][ny] == 1 || map[nx][ny] == 3 || map[nx][ny] ==6 || map[nx][ny] == 7){
-            vis[nx][ny] = vis[x][y]+ 1;
+        if (map[nx][ny] == 1 || map[nx][ny] == 3 || map[nx][ny] == 6 || map[nx][ny] == 7) {
+            vis[nx][ny] = vis[x][y] + 1;
             queue.add(new Node(nx, ny));
         }
     }
 
     private static void checkLeft(int nx, int ny, int x, int y) {
-        if(map[nx][ny] == 1 || map[nx][ny] == 3 || map[nx][ny] ==4 || map[nx][ny] == 5){
-            vis[nx][ny] = vis[x][y]+ 1;
+        if (map[nx][ny] == 1 || map[nx][ny] == 3 || map[nx][ny] == 4 || map[nx][ny] == 5) {
+            vis[nx][ny] = vis[x][y] + 1;
             queue.add(new Node(nx, ny));
         }
     }
-    
+
 
     private static void checkDown(int nx, int ny, int x, int y) {
-        if(map[nx][ny] == 1 || map[nx][ny] == 2 || map[nx][ny] ==4 || map[nx][ny] == 7){
-            vis[nx][ny] = vis[x][y]+ 1;
+        if (map[nx][ny] == 1 || map[nx][ny] == 2 || map[nx][ny] == 4 || map[nx][ny] == 7) {
+            vis[nx][ny] = vis[x][y] + 1;
             queue.add(new Node(nx, ny));
         }
     }
 
     private static void checkUp(int nx, int ny, int x, int y) {
-        if(map[nx][ny] == 1 || map[nx][ny] == 2 || map[nx][ny] ==5 || map[nx][ny] == 6){
-            vis[nx][ny] = vis[x][y]+ 1;
+        if (map[nx][ny] == 1 || map[nx][ny] == 2 || map[nx][ny] == 5 || map[nx][ny] == 6) {
+            vis[nx][ny] = vis[x][y] + 1;
             queue.add(new Node(nx, ny));
         }
     }
