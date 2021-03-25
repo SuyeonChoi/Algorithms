261d875b84c4ce84be65f022d2fb20d881c3f415
SuyeonChoi <tndus970120@gmail.com>
2021-03-25 13:31:41 +0900

    최장 증가 부분 수열 using DP

---
+++ SW Expert Academy/Java/DP/p3307.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p3307 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            int[] dp = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int ans = 0;
+            for (int i = 0; i < N; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (arr[i] >= arr[j] && dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                    }
+                }
+                ans = Math.max(ans, dp[i]);
+            }
+            System.out.println("#" + t + " " + ans);
+        }
+    }
+}
