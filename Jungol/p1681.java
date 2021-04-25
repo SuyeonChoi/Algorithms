a63b7fd6e9f138e8583fbdb8a50f3676c00915f3
SuyeonChoi <tndus970120@gmail.com>
2021-04-25 23:46:59 +0900

    해밀턴 순환회로 using DFS

--- Jungol/p1681.java
+++ Jungol/p1681.java
@@ -7,54 +7,39 @@ import java.util.StringTokenizer;
 
 public class p1681 {
     static int N;
-    static int[][] cost;
+    static int[][] adj;
+    static int answer;
     static boolean[] vis;
-    static int min;
-    static int sum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        cost = new int[N][N];
-
+        adj = new int[N][N];
+        vis = new boolean[N];
+        answer = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                cost[i][j] = Integer.parseInt(st.nextToken());
+                adj[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        min = Integer.MAX_VALUE / 2;
-        for (int i = 1; i < N; i++) {
-            vis = new boolean[N];
-            sum = 0;
-            if (cost[0][i] != 0) {
-                dfs(0, i, 1);
-            }
-        }
-        System.out.println(min);
+
+        vis[0] = true;
+        dfs(0, 1, 0);
+
+        System.out.println(answer);
     }
 
-    private static void dfs(int x, int y, int cnt) {
-        if (sum > min) {
-            return;
-        }
-        sum = sum + cost[x][y];
-        vis[x] = true;
-        if (cnt == (N - 1)) {
-            sum = sum + cost[y][0];
-            if (cost[y][0] == 0) {
-                return;
-            }
-            min = Math.min(min, sum);
-            sum = sum - cost[y][0];
-            return;
-        }
-
-
-        for (int i = 0; i < N; i++) {
-            if (!vis[i] && cost[y][i] != 0 && (sum < min)) {
-                dfs(y, i, cnt + 1);
-                sum = sum - cost[y][i];
+    private static void dfs(int start, int cnt, int sum) {
+        if (sum >= answer) return;
+        if (cnt == N) {
+            if (adj[start][0] == 0) return;
+            answer = Math.min(answer, sum + adj[start][0]);
+        } else {
+            for (int i = 1; i < N; i++) {
+                if (adj[start][i] == 0 || vis[i]) continue;
+                vis[i] = true;
+                dfs(i, cnt + 1, sum + adj[start][i]);
                 vis[i] = false;
             }
         }
