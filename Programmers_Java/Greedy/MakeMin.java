6f0081bd131240fcd2582a5ae70b4fb7652f0583
SuyeonChoi <tndus970120@gmail.com>
2021-06-15 23:25:32 +0900

    최솟값 만들기

---
+++ Programmers_Java/Greedy/MakeMin.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+import java.util.*;
+
+class MakeMin {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int N = A.length;
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            answer += (A[i] * B[N - 1 - i]);
+        }
+
+        return answer;
+    }
+}
